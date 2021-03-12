860cb961287120b95473282d8c2908288bd05ddf
Jason Parr <parr.jason.01@gmail.com>
2021-03-11 16:47:39 -0800

    login form will now validate to the home page.

--- assets/javascript/application.js
+++ assets/javascript/application.js
@@ -1,22 +1,28 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-
-
-  let login = document.querySelector("#form-login");
+/* --------------LOGIN FORM VALIDATION----------------- */
   
+  let login = document.querySelector("#form-login");
+
   function validateField(selector1, selector2) {
     const field1 = document.querySelector(selector1).value;
     const field2 = document.querySelector(selector2).value;
     if (field1 === "jason" && field2 === "1234") {
-      login.style.backgroundColor = "green";
-    }
+      window.location.href = "index.html";
+    } else
+    login.style.backgroundColor = "red";
   }
 
   login.addEventListener("submit", function(event) {
     event.preventDefault();
     validateField("input[id='username']", "input[id='password']" );
-    
    });
+
+
+
+
+
+
 
 
   // Add your Javascript Here
