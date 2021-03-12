a6c93b067a425f96a45cdbb4f09b456016de4bd1
Jason Parr <parr.jason.01@gmail.com>
2021-03-11 16:24:15 -0800

    added username and password validation to login screen, which at this time, only makes the login screen turn green.  prevent default is in effect for submitting the form.

--- assets/javascript/application.js
+++ assets/javascript/application.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", function() {
+
+
+
+  let login = document.querySelector("#form-login");
+  
+  function validateField(selector1, selector2) {
+    const field1 = document.querySelector(selector1).value;
+    const field2 = document.querySelector(selector2).value;
+    if (field1 === "jason" && field2 === "1234") {
+      login.style.backgroundColor = "green";
+    }
+  }
+
+  login.addEventListener("submit", function(event) {
+    event.preventDefault();
+    validateField("input[id='username']", "input[id='password']" );
+    
+   });
+
+
   // Add your Javascript Here
 })
